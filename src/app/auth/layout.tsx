01fb7b5b325723b1cbd2f68835bb56e006229121
Jobie <engrjvramos@gmail.com>
2025-08-12 15:56:53 +0800

    chore: install prisma webpack plugin

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-  return <div className="flex min-h-dvh flex-col items-center justify-center overflow-hidden">{children}</div>;
+  return (
+    <div className="dark:bg-background flex min-h-dvh flex-col items-center justify-center overflow-hidden bg-neutral-200">
+      {children}
+    </div>
+  );
 }
