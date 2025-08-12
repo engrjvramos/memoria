09fe1a050d5b726134da46367164c8a77c792d20
Jobie <engrjvramos@gmail.com>
2025-08-12 16:24:24 +0800

    fix: remove public url from env

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,5 @@
 import { createAuthClient } from 'better-auth/react';
 
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL as string,
-});
+export const authClient = createAuthClient({});
 
 export const { signIn, signOut, signUp, useSession } = authClient;
