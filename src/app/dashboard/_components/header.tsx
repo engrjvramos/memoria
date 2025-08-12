2a25a19862fa34ab7968b4cfc0c9212a41e21e08
Jobie <engrjvramos@gmail.com>
2025-08-12 14:23:40 +0800

    fix: prisma client error

--- src/app/dashboard/_components/header.tsx
+++ src/app/dashboard/_components/header.tsx
@@ -1,4 +1,5 @@
 import ThemeToggler from '@/components/theme-toggler';
+import { Suspense } from 'react';
 import LogoutButton from './logout-button';
 import SearchInput from './search-input';
 
@@ -8,7 +9,9 @@ export default function Header() {
       <div className="flex h-18 items-center justify-between px-8 lg:h-20">
         <h1>All Notes</h1>
         <div className="flex items-center gap-4">
-          <SearchInput />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
           <ThemeToggler />
           <LogoutButton />
         </div>
