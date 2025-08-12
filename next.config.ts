127b5ce79ae2ddb3285fcf019742a77fbe2c26fe
Jobie <engrjvramos@gmail.com>
2025-08-12 16:55:58 +0800

    fix: prisma error

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+//@ts-ignore
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
+const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
